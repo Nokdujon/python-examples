f6b85f6b60ae1405f353c5538e80481c4aa96314
nokdujon <nokdujon@outlook.com>
2024-12-03 16:57:54 +0900

    feat: for-in statement

--- basic_examples.py
+++ basic_examples.py
@@ -1,6 +1,59 @@
 """essential examples"""
 
 
+def loop() -> None:
+    """_for-in statement_"""
+    alphabets: str = "abcdefghijklmnopqrstuvwxyz"
+    chars: list = ["a", "b", "c", "d", "e"]
+    grades: dict = {
+        "A": 90,
+        "B": 80,
+        "C": 70,
+        "D": 60,
+        "F": 59,
+    }
+
+    print("0. iterate in string")
+    for char in alphabets:
+        print(char, end=" ")
+    print("", end="\n\n")
+
+    print("1. iterate in list")
+    for char in chars:
+        print(char, end=" ")
+    print("", end="\n\n")
+
+    print("2. iterate in enumerate(list)")
+    for idx, val in enumerate(chars):
+        print(f"idx = {idx}, val = {val}", end=", ")
+    print("", end="\n\n")
+
+    print("3. iterate in dict.items() and print key and value")
+    for key, val in grades.items():
+        print(f'"{key}": {val}', end=", ")
+    print("", end="\n\n")
+
+    print("4. iterate in dict.items() and print item")
+    for item in grades.items():
+        print(f"item = {item}", end=", ")
+    print("", end="\n\n")
+
+    print("5. iterate in dict and print each pair")
+    for grade in grades:
+        print(f"grade = {grade}", end=", ")
+    print("", end="\n\n")
+
+    print('6. iterate in range(10) and print "hi"')
+    for _ in range(10):
+        print("hi", end=" ")
+    print("", end="\n\n")
+
+    print("7. iterate in range(10, -1, -1), count down!")
+    for idx in range(10, -1, -1):
+        print(idx, end=" ")
+    print("", end="\n\n")
+
+
 def use_print() -> None:
     """_examples of using the print function_"""
 
